7a4c76b586b0689d710547fd1ff3bd018087f2f7
praveensangati <pravin.s731@gmail.com>
2022-06-27 12:46:57 +0530

    Update NACEController.java
    Updated Swagger response with messages

--- src/main/java/com/db/assignment/controller/NACEController.java
+++ src/main/java/com/db/assignment/controller/NACEController.java
@@ -5,6 +5,8 @@ import com.db.assignment.domain.ResponseMessage;
 import com.db.assignment.exception.AssignmentException;
 import com.db.assignment.exception.InvalidFileFormatException;
 import com.db.assignment.service.NACEService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
@@ -15,8 +17,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @RestController
@@ -42,6 +42,11 @@ public class NACEController {
     }
 
     @GetMapping("/{orderId}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Order is created"),
+            @ApiResponse(code = 400, message = "Bad request, adjust details before retrying"),
+            @ApiResponse(code = 500, message = "Internal Server Error, Please check")
+    })
     public ResponseEntity<NomenclatureBean> fetchByOrderId(@PathVariable("orderId") long orderId) {
         log.info("Fetch order by Id");
         NomenclatureBean nomenclatureBean = naceService.getByOrderId(orderId);
